1768ddfda56e17185b0502bd70fdd4aa4a3253e3
X <krishna.telgave@gmail.com>
2022-12-23 13:00:07 +0530

    init

--- src/main/java/com/fss/aeps/Application.java
+++ src/main/java/com/fss/aeps/Application.java
@@ -37,6 +37,7 @@ public class Application {
 	}
 
 	private static final boolean init() {
+		
 		System.setProperty("com.sun.xml.ws.transport.http.client.HttpTransportPipe.dump", "true");
 		System.setProperty("com.sun.xml.internal.ws.transport.http.client.HttpTransportPipe.dump", "true");
 		System.setProperty("com.sun.xml.ws.transport.http.HttpAdapter.dump", "true");
